8cd5c23fb1e2f384f58c98c1c2f52085c5f42560
NAO <Nao@NaoKosaka.local>
2017-02-15 03:10:51 -0500

    added all forms on signup page userinfo stored in the database and added mainpage

--- app/model/User.js
+++ app/model/User.js
@@ -1,37 +1,44 @@
 var Sequelize = require('sequelize')
 
 var attributes = {
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      is: /^[a-z0-9\_\-]+$/i,
+    firstname: {
+        type: Sequelize.STRING
+    },
+    lastname: {
+        type: Sequelize.STRING
+    },
+    birthday: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+    },
+    emailaddress: {
+        type: Sequelize.STRING,
+    },
+    emailaddress2: {
+        type: Sequelize.STRING,
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            is: /^[a-z0-9\_\-]+$/i,
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+    },
+    password2: {
+        type: Sequelize.STRING,
+    },
+    salt: {
+        type: Sequelize.STRING
     }
-  },
-  email: {
-    type: Sequelize.STRING,
-    validate: {
-      isEmail: true
-    }
-  },
-  firstName: {
-    type: Sequelize.STRING,
-  },
-  lastName: {
-    type: Sequelize.STRING,
-  },
-  password: {
-    type: Sequelize.STRING,
-  },
-  salt: {
-    type: Sequelize.STRING
-  }
 }
 
 var options = {
   freezeTableName: true
 }
 
-module.exports.attributes = attributes
-module.exports.options = options
+module.exports.attributes = attributes;
+module.exports.options = options;
