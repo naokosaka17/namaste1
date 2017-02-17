5fda66e36617f8bf4fc7191a0eff7a4f12893492
Danei Stephenson <daneistephenson@Daneis-MacBook-Air.local>
2017-02-16 22:25:59 -0500

    fixed sync issue

--- app/model/models.js
+++ app/model/models.js
@@ -1,8 +1,8 @@
-// var UserMeta = require('./User.js'),
-//     connection = require('../connection.js')
+var UserMeta = require('./Username.js'),
+    connection = require('../connection.js')
 
-// var User = connection.define('users', UserMeta.attributes, UserMeta.options)
+var User = connection.define('users', UserMeta.attributes, UserMeta.options)
 
-// // you can define relationships here
+// you can define relationships here
 
-// module.exports.User = User
+module.exports.User = User
