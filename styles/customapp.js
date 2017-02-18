08d4aece5799833c3f2eeaa8857cbb3b0dcf70e9
Araceli Badillo <badilloaraceli@gmail.com>
2017-02-18 00:06:52 -0500

    updated css and main.handlebars

--- styles/customapp.js
+++ styles/customapp.js
@@ -1,2 +1,8 @@
 $('#first_name').trigger('autoresize');
 $('#last_name').trigger('autoresize');
+
+
+
+document.getElementById("backButton").onclick = function () {
+    location.href = "views/home.handlebars";
+};
